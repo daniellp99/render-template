2b5cf942265b9ca4e0d61437cd506822db5b670a
Daniel Alejandro Leyva Perez <dalejandrolp99@gmail.com>
2023-08-29 16:16:19 -0400

    feat: complete deploy on render tutorial

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+poetry config installer.max-workers 10
+poetry install --no-root --no-interaction --no-ansi -vvv
+
+python manage.py collectstatic --no-input
+python manage.py migrate
